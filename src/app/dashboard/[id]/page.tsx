eebeb618ea22c22d784fb22f91771942880b95ab
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-10-01 17:55:06 -0300

    feat: update page.tsx to generatMetadata according with the contact

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -6,6 +6,7 @@ import { useParams } from "next/navigation";
 import { FaArrowLeft } from "react-icons/fa";
 import { useAuth } from "@/hooks/useAuth";
 import Loading from "@/components/Loading";
+import { Metadata } from "next";
 
 interface Contact {
   id: number;
@@ -17,6 +18,62 @@ interface Contact {
     name: string;
     catchPhrase: string;
   };
+}
+
+async function getContact(id: string): Promise<Contact> {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch contact');
+  }
+  return response.json();
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  try {
+    const contact = await getContact(params.id);
+
+    const title = `${contact.name} - Detalhes do Contato`;
+    const description = `Informações de contato para ${contact.name}`;
+
+    const baseUrl = "https://contact-app-chat2desk.vercel.app"
+
+    const imageUrl = `https://i.pravatar.cc/1200?img=${contact.id}`;
+
+    return {
+      title,
+      description,
+      openGraph: {
+        title,
+        description,
+        type: "profile",
+        url: `${baseUrl}/dashboard/${params.id}`,
+        images: [
+          {
+            url: imageUrl,
+            width: 1200,
+            height: 630,
+            alt: title,
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title,
+        description,
+        images: [imageUrl],
+      },
+    };
+  } catch (error) {
+    console.error("Error generating metadata:", error);
+    return {
+      title: "Detalhes do Contato",
+      description: "Informações do contato não disponíveis no momento.",
+    };
+  }
 }
 
 export default function ContactDetailsPage() {
